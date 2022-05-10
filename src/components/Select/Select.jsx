f466ad89334821db2989d9ffb791216a44f23952
Alex <Aronin.2001@gmail.com>
2022-05-10 21:57:30 +0400

    Added validate for select

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import Errors from '../Errors/Errors';
 
 import './Select.css';
 
-const Select = ({ title, values, selectId }) => {
+const Select = ({ title, values, selectId, errors, name, ...props }) => {
     return (
         <div className="select__container">
-            <label className="select__label" for={selectId}>{title}</label>
-            <select className="select" id={selectId}>
+            <label className="select__label" htmlFor={selectId}>{title}</label>
+            <select className="select" id={selectId} onChange={props.change}>
                 <option className="select__header" value="Country" disabled>{title}</option>
                 {
                     values.map(value => {
                         return <option className="select__value" value={value}>{value}</option>
                     })
                 }
+                {errors[name] ? <Errors error={errors[name]} /> : null}
             </select>
         </div>
 
