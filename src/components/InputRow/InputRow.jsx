cfc1e4870ae5c309d6a871476866c73894b43adc
Alex <Aronin.2001@gmail.com>
2022-05-10 21:56:44 +0400

    Created component InputRow. It's just container for component Input

---
+++ src/components/InputRow/InputRow.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Input from '../Input/Input';
+
+import './InputRow.css';
+
+const InputRow = ({inputs, change, errors}) => {
+    return (
+        <div className="inputs inputs__row">
+            {
+                inputs.map(input => {
+                    return (
+                        <Input key={input.id} title={input.title} type={input.type} name={input.name} inputId={input.name} onChange={change} value={input.value} errors={errors} />
+                    )
+                })
+            }
+
+        </div>
+    )
+};
+
+export default InputRow;
