1225825c59d4a0d1658e32713bf2a40d6870a731
Alex <Aronin.2001@gmail.com>
2022-05-12 23:11:21 +0400

    Added Component Profile

--- src/components/ProfilePage/Profile/Profile.jsx
+++ src/components/ProfilePage/Profile/Profile.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 // default IMG
 import backDefault from '../../../assets/img/avaBackground.jpg';
-
 
 //css
 import './Profile.css';
@@ -11,13 +10,30 @@ import './Profile.css';
 import ProfileBackground from './ProfileBackground/ProfileBackground';
 import ProfileTop from './ProfileTop/ProfileTop';
 import ProfileBottom from './ProfileBottom/ProfileBottom';
+import UserLng from '../../UserLng/UserLng';
 
 const Profile = (props) => {
+    let [isLanguagesBlock, setLanguagesBlock] = useState(false);
+
+    const openLanguagesBlock = () => {
+        setLanguagesBlock(true);
+    }
+
+    const closeLanguagesBlock = () => {
+        setLanguagesBlock(false);
+    }
+
     return (
         <div className="profile main__profile">
             <div className="profile__container">
-                <ProfileTop />
+                <div className="background__coverage" onClick={closeLanguagesBlock}></div>
+                <ProfileTop openLanguagesBlock={openLanguagesBlock} />
                 <ProfileBottom />
+
+                {/* user languages */}
+                {
+                    isLanguagesBlock && <UserLng />
+                }
             </div>
 
             <ProfileBackground backgroundIMG={backDefault} />
