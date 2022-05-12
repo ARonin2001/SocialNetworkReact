60ab7b0f4a00fe0c6f2131a0d0192c9589be8177
Alex <Aronin.2001@gmail.com>
2022-05-12 16:45:04 +0400

    Profile Top

---
+++ src/components/ProfilePage/Profile/ProfileTop/ProfileTop.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import ProfileLanguages from '../ProfileLanguages/ProfileLanguages';
+
+import './ProfileTop.css';
+
+const ProfileTop = (props) => {
+    return (
+        <div className="profile__top">
+            <div className="profile__languages">
+                <ProfileLanguages languages={[{ id: 1, lng: "English" }]} />
+
+                <div className="profile__languages_bottom">
+                    <button className="profile__languages-more">Подробнее...</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProfileTop;
