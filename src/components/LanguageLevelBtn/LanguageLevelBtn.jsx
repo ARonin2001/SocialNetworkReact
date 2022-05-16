630e9995a1308c6504d19edd8add8d9c4d68e3f7
Alex <Aronin.2001@gmail.com>
2022-05-16 22:09:28 +0400

    View languages level

--- src/components/LanguageLevelBtn/LanguageLevelBtn.jsx
+++ src/components/LanguageLevelBtn/LanguageLevelBtn.jsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './LanguageLevelBtn.css';
 
-const LanguageLevelBtn = ({ language, level, classes="" }) => {
+const LanguageLevelBtn = ({ language, level = 1, classes = "" }) => {
+    let [isActiveLevel, setActiveLevel] = useState(false);
+
+    const toggleActiveLevel = () => {
+        if (isActiveLevel)
+            setActiveLevel(false);
+        else
+            setActiveLevel(true);
+    }
+
+    const languagesLevel = [
+        <span className="language__level"></span>,
+        <span className="language__level"></span>,
+        <span className="language__level language__level_active"></span>
+    ];
+
+    // делаем зелёную полосу, если уровень языка совпал с индексом элемента и перевоарчиваем
+    // чтобы отобразить уровни языков в правильном порядке 
+    // (снизу вверх / от меньшего к большему)
+    const reverseLanguagesLevel = languagesLevel.map((item, index) => {
+        if(index < level)
+            return <span className="language__level language__level_active"></span>
+
+        return <span className="language__level"></span>
+    }).reverse();
+
     return (
-        <div className="user-lng__language language__container">
+        <div className={`user-lng__language language__container ${isActiveLevel ? "active" : ""}`}
+            onClick={toggleActiveLevel}>
+
             <div className={`language language__study ${classes}`}>
                 {language}
             </div>
             <div className="language language__levels">
-                <span className="language__level"></span>
-                <span className="language__level"></span>
-                <span className="language__level language__level_active"></span>
+                {
+                    reverseLanguagesLevel
+                }
             </div>
         </div>
     );
