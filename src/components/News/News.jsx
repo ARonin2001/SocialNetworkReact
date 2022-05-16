e0f616e1a0a0cc67c2d877a8f1ff36b8cc43a3c6
Alex <Aronin.2001@gmail.com>
2022-05-16 22:07:32 +0400

    Added arguments for Filter component

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -3,13 +3,15 @@ import Filter from "../Filter/Filter";
 import PostContainer from "../Posts/Post/PostContainer";
 import PostWriter from "../Posts/PostWriter/PostWriter";
 
+import { faHourglassStart, faHourglassEnd, faShuffle } from '@fortawesome/free-solid-svg-icons';
+
 import './News.css';
 
 const MainContent = (props) => {
     return (
         <>
             <PostWriter/>
-            <Filter/>
+            <Filter values={["old news", "new news", "random"]} icons={[faHourglassStart, faHourglassEnd, faShuffle]} />
             <PostContainer/>
         </>
     );
