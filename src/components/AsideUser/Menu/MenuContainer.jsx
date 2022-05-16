d044728e3f0b51f210da7dc31335cdbde91e244f
Alex <Aronin.2001@gmail.com>
2022-05-16 22:12:56 +0400

    I don't remember

--- src/components/AsideUser/Menu/MenuContainer.jsx
+++ src/components/AsideUser/Menu/MenuContainer.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Menu from './Menu';
 import { faHome, faCircleUser, faUserGroup, faMessage, faNewspaper } from "@fortawesome/free-solid-svg-icons";
+import { connect } from 'react-redux';
 
 const MenuContainer = (props) => {
-    const subLiMenuData = [
-        {id: 1, title: "Home", link: "/profile", icon: faHome},
+    let subLiMenuData = [
+        {id: 1, title: "Home", link: `/profile/${props.userId}`, icon: faHome},
         {id: 2, title: "About me", link: "#", icon: faCircleUser },
         {id: 3, title: "Friends", link: "#", icon: faUserGroup},
         {id: 4, title: "Messages", link: "#", icon: faMessage},
         {id: 5, title: "News", link: "/news", icon: faNewspaper},
-    ]
+    ];
 
     return (
         <Menu subLi={subLiMenuData} />
     );
 };
 
-export default MenuContainer;
+let mapStateToProps = state => {
+    return {
+        isAuth: state.auth.isAuth,
+        userId: state.auth.id,
+    }
+};
+
+export default connect(
+    mapStateToProps,
+    {}
+)
+(MenuContainer);
