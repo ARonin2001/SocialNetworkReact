1eec2eecc74ffb952f6c7e04b3b282a755071e4b
Alex <Aronin.2001@gmail.com>
2022-05-26 23:19:03 +0400

    Input for type file for img

--- src/components/Inputs/InputImg/InputImg.jsx
+++ src/components/Inputs/InputImg/InputImg.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 import './InputImg.css';
 
 const InputImg = ({labelTitle, name, ...props}) => {
-    const error  = props.error ? props.error : "";
-
+    const error  = props.error.length ? props.error : "";
+    
     return (
         <div className="input-container">
             <label className="file-label" htmlFor="input-file">{labelTitle}</label>
-            <input type="file" name={name} className="input-file" id="input-file" accept=".png, .jpg, .jpeg" {...props} />
+            <input type="file" name={name} className="input-file" id="input-file" accept="image/png, image/jpg, image/jpeg" {...props} />
             {
-                error && <span className="file-error">error</span>
+                error && <span className="file-error">{error}</span>
             }
         </div>
     );
