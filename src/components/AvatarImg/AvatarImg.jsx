36bffc5ee4ffa557a3ed113bc3007d7a22630b0f
Alex <Aronin.2001@gmail.com>
2022-05-26 23:19:24 +0400

    Avatar img

--- src/components/AvatarImg/AvatarImg.jsx
+++ src/components/AvatarImg/AvatarImg.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
 import avaDefault from '../../assets/img/avaDefault.png';
-
 import './AvatarImg.css';
 
 const AvatarImg = ({ avaIMG }) => {
     const host = window.origin;
-    const avaImg = avaIMG ? host + avaIMG : avaDefault;
+
+    // host 3000 (server host)
+    // Это временное решение
+    let hostServer = host;
+    hostServer = hostServer.substr(0, hostServer.length-4);
+    hostServer += "3000";
+
+    const avaImg = avaIMG ? hostServer + avaIMG : avaDefault;
 
     return (
         <div className="avatar">
