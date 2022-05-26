0ea0a522a5aeee4040ca3f0ede17bc92c9f2395b
Alex <Aronin.2001@gmail.com>
2022-05-26 23:15:35 +0400

    language reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunkMiddleware from 'redux-thunk';
 import authReducer from './reducers/authReducer';
 import profileReducer from './reducers/profileReducer';
+import languagesReducer from './reducers/languagesReducer';
 
 
 let reducers = combineReducers({
     auth: authReducer,
     profilePage: profileReducer,
+    lngReducer: languagesReducer,
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
