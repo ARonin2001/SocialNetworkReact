7bfbeb60cc97cc9803033741a73a5e52a94bf71b
Alex <Aronin.2001@gmail.com>
2022-05-27 23:35:38 +0400

    remove language

--- src/redux/reducers/profileReducer.js
+++ src/redux/reducers/profileReducer.js
@@ -3,6 +3,7 @@ import { usersAPI, profileAPI } from "../../Api/Api";
 const SET_STATUS = "SET-STATUS";
 const SET_USER_DATA = "SET-USER-DATA";
 const ADD_LANGUAGE = "ADD-LANGUAGE";
+const REMOVE_LANGUAGE = "REMOVE-LANGUAGE";
 const UPDATE_AVA = "UPDATE-AVA";
 
 let initialState = {
@@ -29,6 +30,16 @@ let initialState = {
     }
 }
 
+function getLanguagesWithoutDeleteLng(languages, deletedLngId) {
+    return (
+        languages.map(lng => {
+            if(lng._id !== deletedLngId)
+                return {...lng}
+            return lng;
+        })
+    )
+};
+
 const profileReducer = (state = initialState, action) => {
     switch(action.type) {
         case SET_STATUS:
@@ -43,10 +54,13 @@ const profileReducer = (state = initialState, action) => {
                     [action.typeLng]: [...state.profile.languages[action.typeLng], action.languages]} 
                 }
             }
+        case REMOVE_LANGUAGE:
+            return {...state, profile: {...state.profile, languages: {
+                ...state.profile.languages, [action.typeLng]: [...state.profile.languages[action.typeLng], 
+                    getLanguagesWithoutDeleteLng(state.profile.languages[action.typeLng], action.lngId)]
+            }}}
         case UPDATE_AVA:
-            return {...state, profile: {...state.profile, img: {...state.profile.img, ava: action.ava}
-                
-            }}
+            return {...state, profile: {...state.profile, img: {...state.profile.img, ava: action.ava} }}
         default:
             return state;
     };
@@ -57,6 +71,7 @@ export const setStatus = (status) => ({type: SET_STATUS, status});
 export const setUser = (data) => ({type: SET_USER_DATA, data});
 export const addLanguage = (typeLng, languages) => ({type: ADD_LANGUAGE, typeLng, languages});
 export const updateAva = (ava) => ({type: UPDATE_AVA, ava});
+export const removeLanguage = (typeLng, lngId) => ({type: REMOVE_LANGUAGE, typeLng, lngId});
 
 // thukns
 export const updateUserStatus = (userId, status) => {
@@ -79,16 +94,28 @@ export const setProfileData = (userId) => {
     }
 }
 
+// languages
 export const addNewLanguage = (typeLng, lng, userId, level = null) => {
     return async (dispatch) => {
         let response = await profileAPI.addLanguage(typeLng, lng, userId, level);
-        console.log(response);
+
         if(response.status === 200) {
             dispatch(addLanguage(typeLng, response.data.lng));
         }
     }
 }
 
+export const removeUserLanguage = (typeLng, lngId, userId) => {
+    return async (dispatch) => {
+        let response = await profileAPI.removeLanguage(typeLng, lngId, userId);
+
+        if(response.status === 200) {
+            dispatch(removeLanguage(typeLng, userId));
+        }
+    }
+}
+
+// ava
 export const updateProfileAva = (imgName, userId) => {
     return async (dispatch) => {
         let response = await profileAPI.updateAva(imgName, userId);
