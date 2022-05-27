8c59b1885952716bf3c9609b81881a5920eccd04
Alex <Aronin.2001@gmail.com>
2022-05-27 23:39:02 +0400

    Added the element for delete language

--- src/components/LanguageBtn/LanguageBtn.jsx
+++ src/components/LanguageBtn/LanguageBtn.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './LanguageBtn.css';
 
-const LanguageBtn = ({language, classes}) => {
+const LanguageBtn = ({language, classes, deleteMethod}) => {
+    let [isDelete, setDelete] = useState(false);
+
     return (
-        <div className={`language ${classes}`} >
+        <div className={`language ${classes}`} onMouseOver={() => setDelete(true)} 
+            onMouseOut={() => setDelete(false)} >
+            {isDelete && <div className='language_delete' onClick={deleteMethod}></div>}
             {language}
         </div>
     );
