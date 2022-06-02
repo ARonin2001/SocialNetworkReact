bb6f65c7c23cb14a3cc558c0fff3b99ea158330a
Alex <Aronin.2001@gmail.com>
2022-06-03 01:50:57 +0400

    what

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,6 +1,8 @@
 import { headerAPI, usersAPI } from "../../Api/Api";
 
 const SET_USER_DATA = "SET-USER-DATA";
+const ADD_FRIEND = "ADD-FRIEND";
+const REMOVE_FRIEND = "REMOVE-FRIEND";
 
 let initialState = {
     _id: null,
@@ -24,12 +26,18 @@ const authReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_USER_DATA:
             return {...state, ...action.userData, isAuth: action.isAuth }
+        case ADD_FRIEND:
+            return {...state, friends: [...state.friends, action.user]};
+        case REMOVE_FRIEND:
+            return {...state, friends: state.friends.filter(u => u.id !== action.friendId)};
         default:
             return state;
     }
 }
 
 export const setAuthUserData = (userData, isAuth = false) => ({type: SET_USER_DATA, userData, isAuth});
+export const addFriend = (user) => ({type: ADD_FRIEND, user});
+export const removeFriend = (friendId) => ({type: REMOVE_FRIEND, friendId});
 
 export const getAuthUserData = () => {
     return async (dispatch) => {
@@ -65,5 +73,26 @@ export const createNewUser = (userData) => {
         }
     }
 }
+
+export const addNewFriend = (friendId, userId) => {
+    return async(dispatch) => {
+        let response = await usersAPI.addFriend(friendId, userId);
+
+        if(response.status === 200) {
+            dispatch(addFriend(response.data));
+        }
+    }
+}
+
+export const deleteFriend = (friendId, userId) => {
+    return async (dispatch) => {
+        let response = await usersAPI.removeFriend(friendId, userId);
+
+        if(response.status === 200) {
+            dispatch(removeFriend(friendId));
+        }
+    }
+}
+
 
 export default authReducer;
